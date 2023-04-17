94f629cb4161d87c023407835638d2a93f0dcc04
agostinhodev <agostinho.developer@gmail.com>
2023-04-17 11:18:37 -0300

    feat: updated env start

--- start.sh
+++ start.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+DEVELOPMENT=false
+
+for arg in "$@"
+do
+    case $arg in
+        --development)
+        DEVELOPMENT=true
+        shift
+        ;;
+        *)
+        # Unknown option
+        ;;
+    esac
+done
+
 git submodule update --init --recursive --remote
 git submodule foreach --recursive git checkout master
-docker-compose up -d --build
+
+if [ "$DEVELOPMENT" = true ] ; then
+    docker-compose -f docker-compose.development.yml up -d --build
+else
+    docker-compose -f docker-compose.production.yml up -d --build
+fi
